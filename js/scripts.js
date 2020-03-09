938be1d5e7b395cc92da455f884130cb0304a06a
jacquelineswanson <swanson.jacque@gmail.com>
2020-03-08 19:56:47 -0700

    Added Picture/Random Text Generator
    Added Napoleon sketch and random greetings

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,22 @@ const player = new Plyr('#player', {
 
 });
     
+// Store Messages
+let messages = [
+    'You gonna eat your tots?',
+    'Tina you fat lard, come get some dinner!', 
+    'I caught you a delicious bass',
+    'Whatever I feel like I wanna do, GOSH!',
+    'Girls only want boyfriends who have great skills!'
+  ]
+  
+  // Get Random Number
+  var numRand;
+  var max = 5;
+  numRand = Math.floor(Math.random() * max );
+  
+  
+  // Get element
+  let greeting = document.querySelector('h1')
+  // Change element content
+  greeting.innerHTML = messages[ numRand ];
